c14b3bc6459d9f0fdbe55f0c7d9ad60e50e36783
Jia <jiavon@ualberta.ca>
2020-07-27 11:13:02 -0600

    added message

--- 4.1-react-to-do-app/src/App.js
+++ 4.1-react-to-do-app/src/App.js
@@ -1,27 +1,58 @@
 import React from 'react';
 
-
-class App extends React.Component 
+class App extends React.Component
 {
+  constructor ( props )
+  {
+    super( props );
+
+    this.state = {
+      newToDo: "", // Keep track of our new to-do value.
+      toDos: [] // Keep track of all the todos.
+    };
+  }
+
+  addToDo ( event )
+  {
+    event.preventDefault(); // Stop the page from reloading.
+    console.log( "Test add todo!" );
+  }
+
+  updateItem ( key, value )
+  {
+    // We never re-assign the contents of this.state.
+    // this.state is ONLY USED FOR READING VALUES, NOT writing.
+    // When we need to update anything in state, we need to use this.setState()
+    // this.setState() triggers the render() method, so we can see updated state info in our presentation.
+    this.setState( {[key]: value} );
+  }
+
   render ()
   {
     return (
       <>
-      <h1>React To-Do App</h1>
-      <form>
-        <label htmlFor="newToDo">
-          Enter a new "To-Do:"
-          <input type="text" name="newToDo" id="newToDo" required/>
-        </label>
-        <input type ="submit" value="Add New To-Do"/>
-      </form>
-      <h2>Current To-Dos: </h2>
-      <ul>
-        
-      </ul>
+        <h1>React To-Do App</h1>
+        <form onSubmit={this.addToDo}>
+          <label htmlFor="newToDo">
+            Enter a new "To-Do:"
+            <input
+              type="text"
+              name="newToDo"
+              id="newToDo"
+              required
+              value={this.state.newToDo}
+              onChange={event => this.updateItem( 'newToDo', event.target.value )} />
+          </label>
+          <input type="submit" value="Add New To-Do" />
+        </form>
+        <h2>Current To-Dos:</h2>
+        <ul>
+
+        </ul>
       </>
     );
   }
 }
 
 export default App;
+
